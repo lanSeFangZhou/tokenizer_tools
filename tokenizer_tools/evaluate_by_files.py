96ee65fe486e3f2128857aed98db060919290660
Xiaoquan Kong <u1mail2me@gmail.com>
2018-09-06 20:08:46 +0800

    Update code

--- tokenizer_tools/evaluate_by_files.py
+++ tokenizer_tools/evaluate_by_files.py
@@ -2,42 +2,26 @@ from tokenizer_tools.tagset.BMES import BMESEncoderDecoder
 from tokenizer_tools.evaluator.token.tag_level import TagEvaluator
 
 
-def evaluate_by_files(test_file, gold_file, corpus_aligned=False):
+def evaluate_by_files(test_file, gold_file):
     with open(test_file) as fd:
         test_line_list = fd.readlines()
 
     with open(gold_file) as fd:
         gold_line_list = fd.readlines()
 
-    if corpus_aligned:
-        assert len(test_line_list) == len(gold_line_list)
-
     encoder = BMESEncoderDecoder()
     tag_evaluator = TagEvaluator()
 
-    if corpus_aligned:
-        for i in range(len(gold_line_list)):
-            test_line = test_line_list[i]
-            gold_line = gold_line_list[i]
+    test_content = ' '.join([i.strip() for i in test_line_list])
+    gold_content = ' '.join([i.strip() for i in gold_line_list])
 
-            test_word_list = test_line.strip().split()
-            gold_word_list = gold_line.strip().split()
+    test_word_list = test_content.split()
+    gold_word_list = gold_content.split()
 
-            test_tags = encoder.encode_word_list_as_string(test_word_list)
-            gold_tags = encoder.encode_word_list_as_string(gold_word_list)
+    test_tags = encoder.encode_word_list_as_string(test_word_list)
+    gold_tags = encoder.encode_word_list_as_string(gold_word_list)
 
-            tag_evaluator.process_one_batch(gold_tags, test_tags)
-    else:
-        test_content = ' '.join([i.strip() for i in test_line_list])
-        gold_content = ' '.join([i.strip() for i in gold_line_list])
-
-        test_word_list = test_content.split()
-        gold_word_list = gold_content.split()
-
-        test_tags = encoder.encode_word_list_as_string(test_word_list)
-        gold_tags = encoder.encode_word_list_as_string(gold_word_list)
-
-        tag_evaluator.process_one_batch(gold_tags, test_tags)
+    tag_evaluator.process_one_batch(gold_tags, test_tags)
 
     metrics = tag_evaluator.get_score()
 
