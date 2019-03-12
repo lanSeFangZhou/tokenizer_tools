d5a91bb862081b55e9c89c0044e7787822b07c57
Xiaoquan Kong <u1mail2me@gmail.com>
2019-03-12 10:34:24 +0800

    Update requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'scikit-learn', 'scipy', 'tensorflow']
+requirements = ['Click>=6.0', 'scikit-learn', 'scipy', 'tensorflow', 'pandas']
 
 setup_requirements = ['pytest-runner', ]
 
