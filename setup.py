67791c1951f63fe53168261bd90f525a8a98dc67
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-18 14:46:32 +0800

    Bump version: 0.15.2 → 0.15.3

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/howlandersonn/tokenizer_tools",
-    version="0.15.2",
+    version="0.15.3",
     zip_safe=False,
 )
--- tokenizer_tools/__init__.py
+++ tokenizer_tools/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Xiaoquan Kong"""
 __email__ = "u1mail2me@gmail.com"
-__version__ = "0.15.2"
+__version__ = "0.15.3"
