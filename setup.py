8f4e2b840077f4401eddd6647d5cb30e4225a945
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-29 14:57:25 +0800

    Bump version: 0.15.5 → 0.15.6

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/howlandersonn/tokenizer_tools",
-    version="0.15.5",
+    version="0.15.6",
     zip_safe=False,
 )
