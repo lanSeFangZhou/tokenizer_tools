da71878a4b54c997d7c9fd98e661e25139b749e0
Xiaoquan Kong <u1mail2me@gmail.com>
2020-01-20 16:48:28 +0800

    test: fix issue cause by order of file list (relate to glob())

--- tests/conll/test_iterator_reader.py
+++ tests/conll/test_iterator_reader.py
@@ -2,14 +2,14 @@ from tokenizer_tools.conll.iterator_reader import iterator_reader
 
 
 def test_iterator_reader(datadir):
-    input_file = [i for i in datadir.glob("*") if i.is_file()]
+    input_file = list(sorted([i for i in datadir.glob("*") if i.is_file()]))
 
     output = list(iterator_reader(input_file))
 
     expected = [
-        [["3", "c"], ["4", "d"]],
-        [["31", "c1"], ["41", "d1"]],
         [["1", "a"], ["2", "b"]],
         [["11", "a1"], ["21", "b1"]],
+        [["3", "c"], ["4", "d"]],
+        [["31", "c1"], ["41", "d1"]],
     ]
     assert output == expected
